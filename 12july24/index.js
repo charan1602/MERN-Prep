e6bac958d7c22cb9c8f9d427b7dcc1b68eb21cae
charan1602 <charanreddy1602@gmail.com>
2024-07-14 23:58:33 +0530

    asd

---
+++ 12july24/index.js
@@ -0,0 +1,15 @@
+let age=100
+console.log(age)
+let a=20
+let b=10
+
+console.log(a+b)
+console.log(a-b)
+console.log(a/b)
+console.log(a*b)
+console.log(a**2)
+console.log(a>b)
+console.log(a<b)
+console.log(a++)
+console.log(a+b*(a-b))
+console.log(a+b*(a-b))
