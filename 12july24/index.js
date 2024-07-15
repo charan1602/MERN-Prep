24b86e27b3ebadac0240291c2f824da1c19de2f3
charan1602 <charanreddy1602@gmail.com>
2024-07-15 23:53:59 +0530

    ifelse

--- 12july24/index.js
+++ 12july24/index.js
@@ -1,15 +1,44 @@
-let age=100
-console.log(age)
-let a=20
-let b=10
+// let age=100
+// console.log(age)
+// let a=20
+// let b=10
 
-console.log(a+b)
-console.log(a-b)
-console.log(a/b)
-console.log(a*b)
-console.log(a**2)
-console.log(a>b)
-console.log(a<b)
-console.log(a++)
-console.log(a+b*(a-b))
-console.log(a+b*(a-b))
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a/b)
+// console.log(a*b)
+// console.log(a**2)
+// console.log(a>b)
+// console.log(a<b)
+// console.log(a++)
+// console.log(a+b*(a-b))
+let number =  4
+if(number%2 == 0)
+   console.log(number ,"is an even number")
+else
+   console.log(number ,"is an odd number")
+let num = 7
+if(num>5 && num<9)
+    console.log(num ,"is present")
+else
+  console.log(num ,"is not present")
+let x = "h"
+let y = "f"
+if(x == "a" || x == "e" || x == "i" || x == "o" || x == "u"  )
+    console.log(x,"is a vowel")
+else if(y == "a" || y == "e" || y == "i" || y == "o" || y == "u" )
+    console.log(y,"is a vowel")
+else 
+    console.log("no vowels found")
+
+
+    
+let num1 = 10
+if(num1 >= 1 && num1 <= 10)  
+    console.log("num1 is in range 1-10")  
+else if(num1 <=-10 && num1 >= -1)
+    console.log("num1 is in range -1 - -10") 
+else if(num1 >= 200 && num1 <= 300)
+    console.log("num1 is in range 200-300") 
+else 
+console.log("num1 not found in the range") 
