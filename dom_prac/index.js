f8ae7dadfdef9f483e96fb21a794ab0580b096ec
charan1602 <charanreddy1602@gmail.com>
2024-08-04 23:59:14 +0530

    update

--- dom_prac/index.js
+++ dom_prac/index.js
@@ -1,12 +1,6 @@
 let btn = document.getElementById("btn")
 btn.style.backgroundColor = "yellow"
-btn.style.fontFamily = "italic"
-btn.addEventListener("click",()=>{
-    alert("yooooohohohohohohoho")
-})
-let btn = document.getElementById("btn")
-btn.style.backgroundColor = "yellow"
-btn.style.fontFamily = "italic"
+
 btn.addEventListener("click",()=>{
     alert("yooooohohohohohohoho")
 })
